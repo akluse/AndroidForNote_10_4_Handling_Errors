1f0a9dadb5830ddf9095e72aa7c98ae1e82a7130
启辰 <akluse@qq.com>
2019-03-07 21:46:11 +0800

    project init

---
+++ app/src/main/java/com/laicar/androidfornote_10_4_handling_errors/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.laicar.androidfornote_10_4_handling_errors
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
