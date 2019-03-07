3139b15155b77d8b746464d73f60e2c24e30c9ce
启辰 <akluse@qq.com>
2019-03-07 21:48:52 +0800

    update code

--- app/src/main/java/com/laicar/androidfornote_10_4_handling_errors/MainActivity.kt
+++ app/src/main/java/com/laicar/androidfornote_10_4_handling_errors/MainActivity.kt
@@ -2,11 +2,15 @@ package com.laicar.androidfornote_10_4_handling_errors
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val til = findViewById<TextInputLayout>(R.id.username)
+        til.isErrorEnabled=true
+        til.error="You need to enter a name"
     }
 }
